8a4c8ed974251ac0d64420572bc150a5eb5f7a35
Masato Ohba <over.rye@gmail.com>
2022-01-11 22:01:15 +0900

    Remove quotes

--- script.sh
+++ script.sh
@@ -38,7 +38,7 @@ echo '::endgroup::'
 
 echo '::group:: Running brakeman with reviewdog 🐶 ...'
 BRAKEMAN_REPORT_FILE="$TEMP_PATH"/brakeman_report
-brakeman --quiet --format tabs "${INPUT_BRAKEMAN_FLAGS}" --output "$BRAKEMAN_REPORT_FILE"
+brakeman --quiet --format tabs ${INPUT_BRAKEMAN_FLAGS} --output "$BRAKEMAN_REPORT_FILE"
 reviewdog < "$BRAKEMAN_REPORT_FILE" \
   -f=brakeman \
   -name="${INPUT_TOOL_NAME}" \
